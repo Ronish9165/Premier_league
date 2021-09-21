058f7b5b6dfec57f3eeb1cde931a765447daf4f5
Ronish <Sthronesh11@gmail.com>
2021-09-21 09:53:49 +0545

    fifthteen

--- src/library/LoginPage.java
+++ src/library/LoginPage.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
 
 public class LoginPage implements ActionListener {
     JFrame fr;
-    JPanel panel, panel2;
+    JPanel pan, pan2;
     ImageIcon background;
     JLabel lbl_heading,lbl_account,lbl_username,lbl_password, lbl_img;
     JTextField txt_username;
@@ -21,17 +21,17 @@ public class LoginPage implements ActionListener {
     public LoginPage(){
         fr=new JFrame("Login Page");
 
-        panel = new JPanel();
-        panel.setBounds(120, 70, 450, 500);
-        panel.setBackground(Color.white);
-        panel.setBorder(new LineBorder(new Color(0, 0, 0),4));
-        panel.setLayout(null);
-        fr.add(panel);
+        pan = new JPanel();
+        pan.setBounds(120, 70, 450, 500);
+        pan.setBackground(Color.white);
+        pan.setBorder(new LineBorder(new Color(0, 0, 0),4));
+        pan.setLayout(null);
+        fr.add(pan);
 
-        panel2 = new JPanel();
-        panel2.setBounds(0, 0, 800, 700);
-        panel2.setLayout(null);
-        fr.add(panel2);
+        pan2 = new JPanel();
+        pan2.setBounds(0, 0, 800, 700);
+        pan2.setLayout(null);
+        fr.add(pan2);
 
         //Fonts
         fon1=new Font("arial",Font.BOLD,25);
@@ -42,14 +42,14 @@ public class LoginPage implements ActionListener {
         lbl_heading.setFont(fon1);
         lbl_heading.setForeground(Color.BLACK);
         lbl_heading.setBounds(150,10,250,30);
-        panel.add(lbl_heading);
+        pan.add(lbl_heading);
 
         //Label
         lbl_username=new JLabel("Username");
         lbl_username.setFont(fon1);
         lbl_username.setForeground(Color.BLACK);
         lbl_username.setBounds(25,100,120,40);
-        panel.add(lbl_username);
+        pan.add(lbl_username);
 
 
         //label for password
@@ -57,38 +57,38 @@ public class LoginPage implements ActionListener {
         lbl_password.setFont(fon1);
         lbl_password.setForeground(Color.BLACK);
         lbl_password.setBounds(25,190,120,40);
-        panel.add(lbl_password);
+        pan.add(lbl_password);
 
         lbl_account=new JLabel("Don't have an account?");
         lbl_account.setFont(fon2);
         lbl_account.setForeground(Color.BLACK);
         lbl_account.setBounds(30,400,210,40);
-        panel.add(lbl_account);
+        pan.add(lbl_account);
 
         //TextField password
         txt_password=new JPasswordField();
         txt_password.setFont(fon2);
         txt_password.setBounds(155,190,220,40);
-        panel.add(txt_password);
+        pan.add(txt_password);
 
         //TextField
         txt_username=new JTextField();
         txt_username.setFont(fon2);
         txt_username.setBounds(155,100,220,40);
-        panel.add(txt_username);
+        pan.add(txt_username);
 
         //Buttons
         btn_login=new JButton("Login");
         btn_login.setFont(fon1);
         btn_login.setBounds(90,300,150,40);
         btn_login.addActionListener(this);
-        panel.add(btn_login);
+        pan.add(btn_login);
 
         btn_exit=new JButton("Exit");
         btn_exit.setFont(fon1);
         btn_exit.setBounds(270,300,150,40);
         btn_exit.addActionListener(this);
-        panel.add(btn_exit);
+        pan.add(btn_exit);
 
         btn_reg=new JButton("Sign Up");
         btn_reg.setFont(fon1);
@@ -96,13 +96,13 @@ public class LoginPage implements ActionListener {
         btn_reg.setForeground(Color.red);
         btn_reg.setBounds(235,405,150,30);
         btn_reg.addActionListener(this);
-        panel.add(btn_reg);
+        pan.add(btn_reg);
 
         //Image
         background = new ImageIcon(getClass().getResource("bg.png"));
         lbl_img = new JLabel(background);
         lbl_img.setBounds(0, 0, 700, 700);
-        panel2.add(lbl_img);
+        pan2.add(lbl_img);
 
         fr.setSize(700,700);
         fr.setLayout(null);
